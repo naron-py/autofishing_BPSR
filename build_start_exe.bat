2c7ef264d53fd14c18a6045696ecf786a06a6890
naron-py <83053774+naron-py@users.noreply.github.com>
2025-12-29 14:14:39 +0700

    Rollback to pre-UI version

--- build_start_exe.bat
+++
@@ -1,9 +0,0 @@
-@echo off
-setlocal
-
-python -m pip install --upgrade pyinstaller
-pyinstaller --noconsole --onefile --name start start.py
-
-echo.
-echo Build complete. Find start.exe in the "dist" folder.
-pause
